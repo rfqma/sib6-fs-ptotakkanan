3b329c944ac8f2316bbc464d6ce9ae79a448607c
Rifqi Maulana <abelcaster025@gmail.com>
2024-03-07 21:25:52 +0700

    sesi materi tutorial mvc php

--- tugas-mvc/app/views/user/index.php
+++ tugas-mvc/app/views/user/index.php
@@ -1,1 +1,4 @@
-<h1>halaman utama user</h1>
+<div class='container mt-5'>
+  <h1>halaman utama user</h1>
+  <a href="<?= BASE_URL; ?>/user/profile">halaman profile -></a>
+</div>
--- tugas-mvc/app/views/user/profile.php
+++ tugas-mvc/app/views/user/profile.php
@@ -1,6 +1,6 @@
 <div class='container text-center mt-4'>
 <h1>Halaman user</h1>
-<img style='max-width: 40%' src="<?= BASE_URL; ?>/img/profile.jpg" class='rounded-circle shadow'>
+<img style='max-width: 10%' src="<?= BASE_URL; ?>/img/profile.jpg" class='rounded-circle shadow'>
 <p>
   Halo, nama saya 
   <?= $data['nama']; ?>,
